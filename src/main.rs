0fda3b752d862cec15675b6b864b1cd87dd75f41
Razetif <salehzaidan64@gmail.com>
2025-03-24 10:04:13 +0700

    Implement hash-object subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sha1::{Digest, Sha1};
 use std::{env, error::Error, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -9,6 +10,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     match subcommand.as_str() {
         "init" => init()?,
+        "hash-object" => hash_object(args)?,
         _ => return Err(format!("Unknown subcommand: {}", subcommand).into()),
     }
 
@@ -33,6 +35,18 @@ fn init() -> Result<(), Box<dyn Error>> {
     fs::create_dir_all(git_dir_path.join("objects").join("pack"))?;
     fs::create_dir_all(git_dir_path.join("refs").join("heads"))?;
     fs::create_dir_all(git_dir_path.join("refs").join("tags"))?;
+
+    Ok(())
+}
+
+fn hash_object(args: impl Iterator<Item = String>) -> Result<(), Box<dyn Error>> {
+    let files: Vec<String> = args.collect();
+
+    for file in files {
+        let content = fs::read_to_string(file)?;
+        let hash = Sha1::digest(content);
+        println!("{:x}", hash);
+    }
 
     Ok(())
 }
