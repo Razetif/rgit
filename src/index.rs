8d8bf25ce2071d5caee110cc5e3e410040e2daef
Razetif <salehzaidan64@gmail.com>
2025-03-26 01:35:11 +0700

    Implement update-index subcommand

---
+++ src/index.rs
@@ -0,0 +1,189 @@
+use sha1::{Digest, Sha1};
+use std::{
+    error::Error,
+    fs::File,
+    io::{Cursor, Read},
+    os::unix::fs::MetadataExt,
+};
+
+const DEFAULT_VERSION: u32 = 2;
+
+#[derive(Debug)]
+pub struct Index {
+    version: u32,
+    pub entries: Vec<Entry>,
+}
+
+impl Index {
+    pub fn empty() -> Self {
+        Index {
+            version: DEFAULT_VERSION,
+            entries: Vec::new(),
+        }
+    }
+
+    pub fn parse(buf: Vec<u8>) -> Result<Self, Box<dyn Error>> {
+        let mut cursor = Cursor::new(buf);
+        // Skip signature (DIRC)
+        cursor.set_position(4);
+
+        let mut version = [0u8; 4];
+        cursor.read_exact(&mut version)?;
+        let version = u32::from_be_bytes(version);
+
+        let mut entries_len = [0u8; 4];
+        cursor.read_exact(&mut entries_len)?;
+        let entries_len = u32::from_be_bytes(entries_len) as usize;
+
+        let mut entries = Vec::with_capacity(entries_len);
+        for _ in 0..entries_len {
+            let entry = Entry::parse(&mut cursor)?;
+            entries.push(entry);
+        }
+
+        Ok(Index { version, entries })
+    }
+
+    pub fn serialize(&self) -> Result<Vec<u8>, Box<dyn Error>> {
+        let mut contents: Vec<u8> = Vec::new();
+
+        // Header
+        contents.extend("DIRC".as_bytes());
+        contents.extend(self.version.to_be_bytes());
+        let entries_len: u32 = self.entries.len().try_into()?;
+        contents.extend(entries_len.to_be_bytes());
+
+        // Index entries
+        for entry in &self.entries {
+            entry.serialize(&mut contents);
+        }
+
+        let checksum = Sha1::digest(&contents);
+        contents.extend(checksum);
+
+        Ok(contents)
+    }
+}
+
+#[derive(PartialEq, Debug)]
+pub struct Entry {
+    ctime: i64,
+    mtime: i64,
+    dev: u64,
+    ino: u64,
+    mode: u32,
+    uid: u32,
+    gid: u32,
+    size: u64,
+    object_id: [u8; 20],
+    pub filename: String,
+}
+
+impl Entry {
+    pub fn from(filename: &str, file: &mut File) -> Result<Self, Box<dyn Error>> {
+        let mut content = Vec::new();
+        file.read_to_end(&mut content)?;
+        let object_id: [u8; 20] = Sha1::digest(content).try_into()?;
+
+        let metadata = file.metadata()?;
+        Ok(Entry {
+            ctime: metadata.ctime(),
+            mtime: metadata.mtime(),
+            dev: metadata.dev(),
+            ino: metadata.ino(),
+            mode: metadata.mode(),
+            uid: metadata.uid(),
+            gid: metadata.gid(),
+            size: metadata.size(),
+            object_id,
+            filename: String::from(filename),
+        })
+    }
+
+    pub fn parse(cursor: &mut Cursor<Vec<u8>>) -> Result<Self, Box<dyn Error>> {
+        let mut ctime = [0u8; 8];
+        cursor.read_exact(&mut ctime)?;
+        let ctime = i64::from_be_bytes(ctime);
+
+        let mut mtime = [0u8; 8];
+        cursor.read_exact(&mut mtime)?;
+        let mtime = i64::from_be_bytes(mtime);
+
+        let mut dev = [0u8; 8];
+        cursor.read_exact(&mut dev)?;
+        let dev = u64::from_be_bytes(dev);
+
+        let mut ino = [0u8; 8];
+        cursor.read_exact(&mut ino)?;
+        let ino = u64::from_be_bytes(ino);
+
+        let mut mode = [0u8; 4];
+        cursor.read_exact(&mut mode)?;
+        let mode = u32::from_be_bytes(mode);
+
+        let mut uid = [0u8; 4];
+        cursor.read_exact(&mut uid)?;
+        let uid = u32::from_be_bytes(uid);
+
+        let mut gid = [0u8; 4];
+        cursor.read_exact(&mut gid)?;
+        let gid = u32::from_be_bytes(gid);
+
+        let mut size = [0u8; 8];
+        cursor.read_exact(&mut size)?;
+        let size = u64::from_be_bytes(size);
+
+        let mut object_id = [0u8; 20];
+        cursor.read_exact(&mut object_id)?;
+
+        let mut flag = [0u8; 2];
+        cursor.read_exact(&mut flag)?;
+        let flag = u16::from_be_bytes(flag);
+        let filename_len = usize::try_from(flag & 0xFFF)?;
+
+        let mut filename = Vec::with_capacity(filename_len);
+        cursor
+            .take(filename_len as u64)
+            .read_to_end(&mut filename)?;
+        let filename = String::from_utf8(filename)?;
+
+        Ok(Entry {
+            ctime,
+            mtime,
+            dev,
+            ino,
+            mode,
+            uid,
+            gid,
+            size,
+            object_id,
+            filename,
+        })
+    }
+
+    pub fn serialize(&self, buf: &mut Vec<u8>) {
+        buf.extend(self.ctime.to_be_bytes());
+        buf.extend(self.mtime.to_be_bytes());
+        buf.extend(self.dev.to_be_bytes());
+        buf.extend(self.ino.to_be_bytes());
+        buf.extend(self.mode.to_be_bytes());
+        buf.extend(self.uid.to_be_bytes());
+        buf.extend(self.gid.to_be_bytes());
+        buf.extend(self.size.to_be_bytes());
+        buf.extend(self.object_id);
+
+        let mut flag: u16 = 0;
+        if self.filename.len() < 0xFFF {
+            flag |= self.filename.len() as u16;
+        } else {
+            flag |= 0xFFF;
+        }
+        buf.extend(flag.to_be_bytes());
+
+        buf.extend(self.filename.as_bytes());
+        buf.push(0);
+        let padding_len = (8 - (buf.len() % 8)) % 8;
+        let padding = vec![0; padding_len];
+        buf.extend(padding);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 use clap::{Args, Parser, Subcommand};
 use flate2::{Compression, read::ZlibDecoder, write::ZlibEncoder};
+use index::{Entry, Index};
 use sha1::{Digest, Sha1};
 use std::{
     error::Error,
-    fs,
-    io::{self, Read, Write},
+    fs::{self, File, OpenOptions, metadata},
+    io::{self, Read, Seek, SeekFrom, Write},
+    os::unix::fs::MetadataExt,
     path::{self, PathBuf},
 };
+
+mod index;
 
 const GIT_DIR: &str = ".rgit";
 const OBJECTS_DIR: &str = "objects";
@@ -24,6 +28,7 @@ pub enum Commands {
     Init,
     HashObject(HashObjectArgs),
     CatFile(CatFileArgs),
+    UpdateIndex(UpdateIndexArgs),
 }
 
 #[derive(Args, Debug)]
@@ -51,12 +56,27 @@ pub struct CatFileArgs {
     object: String,
 }
 
+#[derive(Args, Debug)]
+pub struct UpdateIndexArgs {
+    #[arg(long = "add")]
+    add: bool,
+
+    #[arg(long = "remove")]
+    remove: bool,
+
+    #[arg(long = "verbose")]
+    verbose: bool,
+
+    files: Vec<PathBuf>,
+}
+
 impl Commands {
     pub fn run(&self) -> Result<(), Box<dyn Error>> {
         match self {
             Self::Init => self.init(),
             Self::HashObject(args) => self.hash_object(&args),
             Self::CatFile(args) => self.cat_file(&args),
+            Self::UpdateIndex(args) => self.update_index(&args),
         }
     }
 
@@ -147,6 +167,62 @@ impl Commands {
                 _ => todo!(),
             }
         }
+
+        Ok(())
+    }
+
+    fn update_index(&self, args: &UpdateIndexArgs) -> Result<(), Box<dyn Error>> {
+        let index_path = PathBuf::from(GIT_DIR).join("index");
+        let mut index_file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(&index_path)?;
+        let mut index = if metadata(index_path)?.size() == 0 {
+            Index::empty()
+        } else {
+            let mut buf = Vec::new();
+            index_file.read_to_end(&mut buf)?;
+            Index::parse(buf)?
+        };
+
+        let entries: Vec<_> = args
+            .files
+            .iter()
+            .map(|filename| {
+                let mut file = File::open(filename)?;
+                let entry = Entry::from(
+                    filename.to_str().ok_or_else(|| "Malformed input")?,
+                    &mut file,
+                );
+                entry
+            })
+            .collect::<Result<_, _>>()?;
+        for entry in entries {
+            if args.remove {
+                if index.entries.contains(&entry) {
+                    index.entries.retain(|e| *e != entry);
+                    if args.verbose {
+                        println!("remove '{}'", entry.filename);
+                    }
+                }
+            }
+
+            if args.add {
+                if !index.entries.contains(&entry) {
+                    let filename = entry.filename.clone();
+                    index.entries.push(entry);
+                    if args.verbose {
+                        println!("add '{}'", filename);
+                    }
+                }
+            }
+        }
+
+        let content = index.serialize()?;
+        index_file.seek(SeekFrom::Start(0))?;
+        index_file.write_all(&content)?;
+        index_file.set_len(content.len() as u64)?;
 
         Ok(())
     }
